97a09b219b9e0d9aead1c182f28f050777bd4a34
Thiago <tadriano.net@gmail.com>
2017-11-26 14:29:08 -0200

    first commit

---
+++ src/app/pages/about/about.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
+})
+export class AboutComponent implements OnInit {
+
+  constructor(private route: ActivatedRoute) { 
+    this.route.params.subscribe(res => console.log(res.id));
+  }
+
+  
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/services/auth/auth.guard.ts
@@ -0,0 +1,26 @@
+import { Observable } from 'rxjs/Observable';
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from "@angular/router";
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+
+    constructor(private router: Router) { }
+
+    canActivate(
+        route: ActivatedRouteSnapshot,
+        state: RouterStateSnapshot): Observable<boolean> | boolean {
+
+        if (localStorage['token'] != null) {
+            return true;
+        } else {
+            console.log('Error 401');
+            this.router.navigate(['/login']);
+        }
+
+    }
+
+
+
+
+}
